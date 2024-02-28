ad227b972bfe324b39ac3a65db9e93eb3adb32c4
Leli2004 <danielidondallavecchia@gmail.com>
2024-02-28 18:22:46 -0300

    Projeto Flask com IA de tradução do Azure

--- Projetos/IA-python/my-venv/Scripts/activate.bat
+++ Projetos/IA-python/my-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Codar\Bootcamp-python-django\Meus_projetos\IA-python\my-venv
+set VIRTUAL_ENV=C:\Codar\Bootcamp-python-django\Projetos\IA-python\my-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
